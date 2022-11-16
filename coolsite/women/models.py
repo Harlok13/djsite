bccb422c3f0d062f2abbf9d11be55371e3f588dc
Harlok <holmik90@yandex.ru>
2022-11-16 23:57:27 +0300

    add class Meta

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -5,13 +5,20 @@ from django.urls import reverse
 class Women(models.Model):  # последовательность полей будет такая же, как мы укажем ее здесь
     """Девушки"""
     title = models.CharField(max_length=250, verbose_name='Заголовок')
-    content = models.TextField(blank=True, verbose_name='Текст статьи')  # 1ый аргумент означает, что поле может быть пустым
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name='Фото')  # чтобы Джанго имел возможность выполнять операции загрузки
+    content = models.TextField(blank=True,
+                               verbose_name='Текст статьи')  # 1ый аргумент означает, что поле может быть пустым
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d",
+                              verbose_name='Фото')  # чтобы Джанго имел возможность выполнять операции загрузки
     # в настройках необходимо прописать следующие константы MEDIA_ROOT и MEDIA_URL
-    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')  # 1ый аргумент будет использоваться один раз при создании записи
-    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')  # 1ый аргумент будет использоваться каждый раз при изменении записи
-    is_published = models.BooleanField(default=True, verbose_name='Публикация')  # 1ый аргумент будет отображать опубликована ли запись
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Каталог')  # 1 аргумент в кавычках, потому что модель
+    time_create = models.DateTimeField(auto_now_add=True,
+                                       verbose_name='Время создания')  # 1ый аргумент будет использоваться один раз при создании записи
+    time_update = models.DateTimeField(auto_now=True,
+                                       verbose_name='Время изменения')  # 1ый аргумент будет использоваться каждый раз при изменении записи
+    is_published = models.BooleanField(default=True,
+                                       verbose_name='Публикация')  # 1ый аргумент будет отображать опубликована ли запись
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True,
+                            verbose_name='Каталог')  # 1 аргумент в кавычках, потому что модель
+
     # Category находится ниже модели Women. 3ий аргумент прописывается, потому что поле нужно заполнить, а модель
     # Category еще даже не была создана
 
@@ -26,9 +33,11 @@ class Women(models.Model):  # последовательность полей б
         verbose_name_plural = 'Известные женщины'
         ordering = ['time_create', 'title']  # сортировка (на сайте не сортируется)
 
+
 class Category(models.Model):
     """Категории"""
     name = models.CharField(max_length=100, db_index=True)  # второй аргумент означает, что поле будет индексируемым,
+
     # т.е. поиск по нему будет происходить быстрее
 
     def __str__(self):
@@ -36,3 +45,8 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
