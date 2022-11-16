1aa61696e400cb9f1730fd6566435def497fa86a
Harlok <holmik90@yandex.ru>
2022-11-16 23:55:28 +0300

    add class WomenAdmin and CategoryAdmin

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from women.models import Women, Category
 
 
-# class WomenAdmin(admin.ModelAdmin):
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')  # отображает поля, которые мы хотим увидеть
+    list_display_links = ('id', 'title')  # поля, на которые мы можем кликнуть и перейти на соответствующую статью для
+    # ее редактирования
+    search_fields = ('title', 'content')  # по каким полям можно производить поиск
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)  # обязательна запятая, потому что это кортеж
 
-admin.site.register(Women)
-admin.site.register(Category)
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -4,14 +4,14 @@ from django.urls import reverse
 
 class Women(models.Model):  # последовательность полей будет такая же, как мы укажем ее здесь
     """Девушки"""
-    title = models.CharField(max_length=250)
-    content = models.TextField(blank=True)  # 1ый аргумент означает, что поле может быть пустым
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")  # чтобы Джанго имел возможность выполнять операции загрузки
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')  # 1ый аргумент означает, что поле может быть пустым
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name='Фото')  # чтобы Джанго имел возможность выполнять операции загрузки
     # в настройках необходимо прописать следующие константы MEDIA_ROOT и MEDIA_URL
-    time_create = models.DateTimeField(auto_now_add=True)  # 1ый аргумент будет использоваться один раз при создании записи
-    time_update = models.DateTimeField(auto_now=True)  # 1ый аргумент будет использоваться каждый раз при изменении записи
-    is_published = models.BooleanField(default=True)  # 1ый аргумент будет отображать опубликована ли запись
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)  # 1 аргумент в кавычках, потому что модель
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')  # 1ый аргумент будет использоваться один раз при создании записи
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')  # 1ый аргумент будет использоваться каждый раз при изменении записи
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')  # 1ый аргумент будет отображать опубликована ли запись
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Каталог')  # 1 аргумент в кавычках, потому что модель
     # Category находится ниже модели Women. 3ий аргумент прописывается, потому что поле нужно заполнить, а модель
     # Category еще даже не была создана
 
@@ -24,7 +24,7 @@ class Women(models.Model):  # последовательность полей б
     class Meta:
         verbose_name = 'Известные женщины'
         verbose_name_plural = 'Известные женщины'
-        ordering = ['time_create', 'title']  # сортировка (на сайте не сортируюется)
+        ordering = ['time_create', 'title']  # сортировка (на сайте не сортируется)
 
 class Category(models.Model):
     """Категории"""
