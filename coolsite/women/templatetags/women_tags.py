d80ba768668d25fbf428b7401c5583b725a51090
Harlok <holmik90@yandex.ru>
2022-11-17 13:40:18 +0300

    create list_categories.html

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -4,11 +4,17 @@ from women.models import Category
 register = template.Library()  # регистрация собственных тегов
 
 @register.simple_tag(name='getcats')  # связывание функции с тегом
-def get_categories():
-    return Category.objects.all()  # возвращение всех записей этой модели в виде коллекций
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)  # возвращение всех записей этой модели в виде коллекций
 
 @register.inclusion_tag('women/tags/list_categories.html')
-def show_categories():
-    cats = Category.objects.all()
-    return {'cats': cats}
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+    return {'cats': cats, 'cat_selected': cat_selected}
 
