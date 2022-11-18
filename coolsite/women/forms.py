5e319b4604f1c7fc9931f54403d8e57486c7f073
Harlok <holmik90@yandex.ru>
2022-11-19 02:12:14 +0300

    add form html

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -3,7 +3,7 @@ from .models import *
 
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_lengtx=255)
+    slug = forms.SlugField(max_length=255)
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
     is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all)  # будет показывать соответ. список, где мы будем выбирать категорию
+    cat = forms.ModelChoiceField(queryset=Category.objects.all())  # будет показывать соответ. список, где мы будем выбирать категорию
