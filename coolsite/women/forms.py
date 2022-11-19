2e2718d5c30be5f1ce5f714c69f4bd7063ecd9a2
Harlok <holmik90@yandex.ru>
2022-11-19 17:10:19 +0300

    change AddPostForm

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import *
 
 # class AddPostForm(forms.Form):
@@ -10,15 +12,21 @@ from .models import *
 # поля с датами здесь не указываются, потому что они сами создаются при создании записи в бд
 
 class AddPostForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['cat'].empty_label = 'Категория не выбрана'
+    def __init__(self, *args, **kwargs):  # вызывает конструктор
+        super().__init__(*args, **kwargs)  # вызывает конструктор базового класса, после него появится поле
+        self.fields['cat'].empty_label = 'Категория не выбрана'  # меняет поле cat
 
     class Meta:
         model = Women  # делает связь этой формы с моделью
-        fields = ['title', 'slug', 'content', 'is_published', 'cat']  # какие поля нужно отобразить в форме
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']  # какие поля нужно отобразить в форме
         widgets = {
            'title': forms.TextInput(attrs={'class': 'form-input'}),
            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
         }
         # ('__all__' отобразит все поля, кроме тех, что указываются автоматически)
+
+    def clean_title(self):
+        title = self.cleaned_data['title']  # получаем данные по заголовку из коллекции cleaned_data, которая доступна в экземпляре класса AddPostForm
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
+        return title
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -25,14 +25,15 @@ def about(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            # try:
+            form.save()
+                # Women.objects.create(**form.cleaned_data)  # для формы без привязки к бд
+            return redirect('home')
+            # except:
+            #     form.add_error(None, 'Ошибка добавления поста')
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'title': 'Добавление статьи', 'form': form})
